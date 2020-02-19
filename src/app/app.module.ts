8b0774899d6a24e216748c39725867eed0b7f7b9
crisormer <cristhianortizmercado@gmail.com>
2020-02-18 21:49:54 -0400

    primer commint con todo lo del proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { FrutaComponent } from './fruta/fruta.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { routing, appRoutingProviders } from './app.routing';
+import { ContactoComponent } from './contacto/contacto.component';
+import { HomeComponent } from './home/home.component';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FrutaComponent,
+    EmpleadosComponent,
+    HomeComponent,
+    ContactoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    routing
+  ],
+  providers: [appRoutingProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

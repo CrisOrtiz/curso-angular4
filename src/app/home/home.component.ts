207cdc536186d642c00f92d81fab1d53cac1f64a
CrisOrtiz <cristhianortizmercado@gmail.com>
2020-02-19 11:46:53 -0400

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,25 @@ import { RopaService } from './../services/ropa.service';
 
 export class HomeComponent{
     public titulo = "Pagina  principal";
+    public listado_ropa:Array<string>;
+    public prenda_a_guardar:string;
 
     constructor(
         private _ropaService: RopaService
     ){}
 
     ngOnInit(){
+        console.log( this.listado_ropa = this._ropaService.getropa());
         console.log(this._ropaService.prueba());
+    }
+
+    guardarPrenda(){
+        this._ropaService.addRopa(this.prenda_a_guardar);
+        this.prenda_a_guardar =null;
+    }
+
+    eliminarPrenda(index:number){
+        this._ropaService.deleteRopa(index);
     }
 }
 
--- src/app/services/ropa.service.ts
+++ src/app/services/ropa.service.ts
@@ -3,8 +3,26 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RopaService {
   public nombre_prenda = 'Camiseta Nike';
+  public coleccion_ropa = ['Pantalones Blancos', 'Camiseta Roja'];
 
-  prueba(){
+  prueba() {
     return this.nombre_prenda;
   }
+
+  addRopa(nombre_prenda: string): Array<string> {
+    this.coleccion_ropa.push(nombre_prenda);
+
+    return this.getropa();
+  }
+
+  deleteRopa(index: number) {
+    this.coleccion_ropa.splice(index, 1);
+
+    return this.getropa();
+  }
+
+  getropa(): Array<string> {
+    return this.coleccion_ropa;
+  }
+
 }

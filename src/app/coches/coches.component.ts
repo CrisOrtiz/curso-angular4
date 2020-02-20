a8befdf3ec5b631f2c485e81ea6013b764dcbbc1
CrisOrtiz <cristhianortizmercado@gmail.com>
2020-02-19 21:54:48 -0400

    commit

---
+++ src/app/coches/coches.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'coches',
+  templateUrl: './coches.component.html'
+})
+export class CochesComponent {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

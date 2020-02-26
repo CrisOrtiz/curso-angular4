ff87cd00073277027ea48de5a14c6e7dab57068c
CrisOrtiz <cristhianortizmercado@gmail.com>
2020-02-26 16:22:30 -0400

    commit

--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -1,26 +1,36 @@
+import { PeticionesService } from '../services/peticiones.service';
 import { Component } from '@angular/core';
 import { Coche } from './coche';
 
 
 @Component({
   selector: 'coches',
-  templateUrl: './coches.component.html'
+  templateUrl: './coches.component.html',
+  providers: [PeticionesService]
 })
 export class CochesComponent {
   public coche: Coche;
-  public coches:Array<Coche>;
+  public coches: Array<Coche>;
 
-  constructor() {
+
+  constructor(
+    private _peticionesService: PeticionesService
+  ) {
     this.coche = new Coche("", "", "");
     this.coches = [
-      new Coche("Seat Panda","120","Blanco"),
-      new Coche("Renault Clio","110","Rojo"),
+      new Coche("Seat Panda", "120", "Blanco"),
+      new Coche("Renault Clio", "110", "Rojo"),
     ];
+
   }
 
-  onSubmit(){
+  ngOnInit() {
+    console.log(this._peticionesService.getPrueba())
+  }
+
+  onSubmit() {
     this.coches.push(this.coche);
     console.log(this.coche);
-    this.coche=new Coche("", "", "");
+    this.coche = new Coche("", "", "");
   }
 }
---
+++ src/app/services/peticiones.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class PeticionesService {
+    public url:string;
+
+    constructor(){
+        this.url="https://jsonplaceholder.typicode.com/posts";
+    }
+    getPrueba() {
+        return 'Hola mundo desde el Servicio';
+        
+    }
+}
+
+
+
+//correccion de version de angular usar map asi ---> .pipe(map(res => res.json())); 
+
